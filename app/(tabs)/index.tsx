6a7affdc678317815e1e287ede962ef15ea292d1
flavius <flaviuscojocaru19@gmail.com>
2025-06-03 15:53:53 +0300

    feat: movie card, homepage done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,6 +12,7 @@ import {
   Text,
   View,
 } from "react-native";
+import MovieCard from "../components/MovieCard";
 import SearchBar from "../components/SearchBar";
 
 export default function Index() {
@@ -54,9 +55,7 @@ export default function Index() {
 
               <FlatList
                 data={movies}
-                renderItem={({ item }) => (
-                  <Text className="text-white text-sm">{item.title}</Text>
-                )}
+                renderItem={({ item }) => <MovieCard {...item} />}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={3}
                 columnWrapperStyle={{
